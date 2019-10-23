203d6a91668ccd8163b2adee70d9afa819e9eeeb
Dell <Dell@DESKTOP-TG798OA>
2019-10-22 20:59:01 -0400

    new repo for test code

---
+++ src/arithmetic/ArithmeticBase.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmetic;
+
+import java.util.Scanner;
+
+/** This class takes String input plus,minus,divide and times
+ * from user and execute the arithmetic operation
+ * change the code to use enum instead String and add object oriented principles satisfy as comment in the code
+ * @author sivagamasrinivasan
+ * date 10/22
+ */
+public class ArithmeticBase 
+{
+ public double x,y;
+ 
+    double calculate(double x, double y){ 
+        enum = {PLUS,MINUS,TIMES,DIVIDE};
+        
+       
+    
+   
+        switch()
+        {
+            case "PLUS":
+                return x + y;
+            case "MINUS":
+                return x - y;
+            case "TIMES":
+                return x * y;
+            case "DIVIDE":
+                return x / y;
+            default:
+                throw new AssertionError("Unknown operations " + this);
+        }
+    }
+   
+}
